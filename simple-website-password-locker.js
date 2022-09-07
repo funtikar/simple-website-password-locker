d7b015686893107f2c729db97dff93d7090da107
funtikar <funtikar@gmail.com>
2022-09-07 09:00:38 +0800

    Update simple-website-password-locker.js
    change the whole input page for the sake of UX lol

--- simple-website-password-locker.js
+++ simple-website-password-locker.js
@@ -6,13 +6,16 @@
 // @author       AzmoRamon
 // @match        https://web.whatsapp.com/*
 // @match        https://keep.google.com/*
+// @match        https://mail.google.com/*
 // @grant        none
 // ==/UserScript==
 /* eslint-env jquery */
 (function() {
     'use strict';
     var pinDiv = document.createElement("div");
-    var pin = "pokol";
+    document.body.appendChild(pinDiv);
+
+    var pin = "femon";
     var timeoutMinutes = 1.5;
 
     var idleTimer = null;
@@ -26,61 +29,48 @@
     document.addEventListener("keypress", function () {
         clearInterval(idleTimer);
         setupInterval();
+        if (event.key === "Enter") {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            document.getElementById("authOK").click();
+  }
     }, false);
-
-
-
-
+    window.document.getElementById("thePass").focus();
 
     function setupInterval() {
         idleTimer = setInterval(hide, timeoutMinutes * 60000);
     }
-
+function cubafokus() {
+            window.document.getElementById("thePass").focus();
+        }
     function hide() {
 
-        //var pinDiv = document.createElement("div");
+        var thisWindow = window;
+        var theHTMLA =""
         pinDiv.style = "position: fixed; z-index: 9999; top: 0px; left: 0px; width: 100%; height: 100%; background-color: #fff";
-        pinDiv.innerHTML = ``;
-        pinDiv.addEventListener('click', prontog, true);
-        document.body.appendChild(pinDiv);
+        theHTMLA += "<h3>Timeout has happen</h3>";
+        theHTMLA += "Password: <input type='password' id='thePass' placeholder='Enter Password...'/>";
+        theHTMLA += "<br />";
+        theHTMLA += "<input type='button' value='OK' id='authOK' hidden />"
+        pinDiv.innerHTML = theHTMLA;
+        var thePass = thisWindow.document.getElementById("thePass").value;
+        window.document.getElementById("thePass").addEventListener('focusout',cubafokus )
+        window.addEventListener('load', function () {
+            window.document.getElementById("thePass").focus();
+        })
+        thisWindow.document.getElementById("authOK").onclick = function () {
+            doAuthentication(thisWindow,thisWindow.document.getElementById("thePass").value);
+        }
+
 
     }
-    function unloker() {
-        var sheesh = prompt("what password?");
 
-        if (sheesh == pin) {
-                //alert("truee")
-                pinDiv.style = "position: fixed; z-index: 0; top: 0px; left: 0px; width: 0%; height: 0%; background-color: #fff";
-                //document.body.removeChild(pinDiv);
-
-        }
-        }
-    function prontog() {
-        var thePrompt = window.open("", "", "left=512,top=340,width=300,height=200");
-
-        var theHTML = "";
-        //theHTML +="<head><link rel=”stylesheet” href=”https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css”rel=”nofollow” integrity=”sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I” crossorigin=”anonymous”></head>"
-        theHTML += "<h3>Timeout has happen</h3>";
-        theHTML += "<br/>";
-        theHTML += "Password: <input type='password' id='thePass' placeholder='Enter Password...'/>";
-        theHTML += "<br />";
-        theHTML += "<input type='button' value='OK' id='authOK'/>"
-        theHTML += "<title>Login Page</title>";
-        thePrompt.document.body.innerHTML = theHTML;
-
-        //var theUser = thePrompt.document.getElementById("theUser").value;
-        var thePass = thePrompt.document.getElementById("thePass").value;
-        thePrompt.document.getElementById("authOK").onclick = function () {
-            doAuthentication(thePrompt,thePrompt.document.getElementById("thePass").value);
-        }
-    }
     function doAuthentication(wodo, pass) {
         if (pass==pin){
             pinDiv.style = "position: fixed; z-index: 0; top: 0px; left: 0px; width: 0%; height: 0%; background-color: #fff";
+            window.document.getElementById("thePass").removeEventListener('focusout', cubafokus)
         }
-        wodo.close();
-
-
     }
 
 
